5677cf70f6b9070a42f956a7bc3dd6bbbf06feff
squidmania <squidman@gmail.com>
2018-09-29 15:29:07 +0100

    continue page styling

--- dom.js
+++ dom.js
@@ -2,10 +2,10 @@ var start = document.querySelector("#start");
 var intro = document.querySelector("#intro");
 var main = document.querySelector("#main");
 start.addEventListener("click", function() {
-  intro.style.visibility = "hidden";
-  intro.style.display = "none";
-  main.style.visibility = "visible";
-  main.style.display = "flex";
+    intro.style.visibility = "hidden";
+    intro.style.display = "none";
+    main.style.visibility = "visible";
+    main.style.display = "flex";
 });
 
 var table = timesTables.table();
@@ -19,24 +19,24 @@ var ansButton = document.querySelector("#submit");
 var form = document.querySelector("#answer-form");
 var i = 0;
 form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  if (timesTables.check(shuffled[i], answer.value) == true) {
-    var answerBox = document.querySelector(
-      ".box" + timesTables.box(shuffled[i])
-    );
-    answerBox.setAttribute("style", "background-color: #09bc8a");
-    i += 1;
-    q.textContent = `${shuffled[i][0]}x${shuffled[i][1]}`;
-    answer.value = "";
-    answer.focus();
-  } else {
-    var answerBox = document.querySelector(
-      ".box" + timesTables.box(shuffled[i])
-    );
-    answerBox.setAttribute("style", "background-color: #d8315b");
-    i += 1;
-    q.textContent = `${shuffled[i][0]}x${shuffled[i][1]}`;
-    answer.value = "";
-    answer.focus();
-  }
+    event.preventDefault();
+    if (timesTables.check(shuffled[i], answer.value) == true) {
+        var answerBox = document.querySelector(
+            ".box" + timesTables.box(shuffled[i])
+        );
+        answerBox.setAttribute("style", "background-color: #09bc8a");
+        i += 1;
+        q.textContent = `${shuffled[i][0]}x${shuffled[i][1]}`;
+        answer.value = "";
+        answer.focus();
+    } else {
+        var answerBox = document.querySelector(
+            ".box" + timesTables.box(shuffled[i])
+        );
+        answerBox.setAttribute("style", "background-color: #d8315b");
+        i += 1;
+        q.textContent = `${shuffled[i][0]}x${shuffled[i][1]}`;
+        answer.value = "";
+        answer.focus();
+    }
 });
